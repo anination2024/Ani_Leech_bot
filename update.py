690064411258dbb1ee28d57822c551180d66c9bf
anination2024 <skypictures19@gmail.com>
2024-10-02 18:09:19 +0530

    Update update.py

--- update.py
+++ update.py
@@ -29,12 +29,12 @@ except:
 
 load_dotenv('config.env', override=True)
 
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7558068681:AAEcJPjuhdqDvYJeyf9iqLYngMcBf9vWWnc')
 if len(BOT_TOKEN) == 0:
     error("BOT_TOKEN variable is missing! Exiting now")
     exit(1)
 
-bot_id = BOT_TOKEN.split(':', 1)[0]
+bot_id = BOT_TOKEN.split('7558068681:AAEcJPjuhdqDvYJeyf9iqLYngMcBf9vWWnc', 1)[0]
 
 DATABASE_URL = environ.get('DATABASE_URL', '')
 if len(DATABASE_URL) == 0:
